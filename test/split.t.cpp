ab77b355fdfb2755bf8a28a95aab34ff2d652d62
Jeremy Fixemer <jeremyfixemer@gmail.com>
2020-05-23 18:48:07 -0500

    Add install to cmake. Add more splitting functions.

--- test/split.t.cpp
+++ test/split.t.cpp
@@ -26,4 +26,72 @@ TEST_CASE("split around range")
     REQUIRE(split_around("abc"_sv, 2) == make_split("ab"_sv, ""_sv));
     REQUIRE(split_around("abc"_sv, 3) == make_split("abc"_sv, ""_sv));
 }
+
+
+TEST_CASE("split at position")
+{
+    REQUIRE(split_at(""_sv, 0) == make_split(""_sv, ""_sv));
+    REQUIRE(split_at(""_sv, 1) == make_split(""_sv, ""_sv));
+    REQUIRE(split_at("abc"_sv, 0) == make_split(""_sv, "abc"_sv));
+    REQUIRE(split_at("abc"_sv, 1) == make_split("a"_sv, "bc"_sv));
+    REQUIRE(split_at("abc"_sv, 2) == make_split("ab"_sv, "c"_sv));
+    REQUIRE(split_at("abc"_sv, 3) == make_split("abc"_sv, ""_sv));
+    REQUIRE(split_at("abc"_sv, 4) == make_split("abc"_sv, ""_sv));
+}
+
+TEST_CASE("split on a char, leave delimiter in second part")
+{
+    REQUIRE(split_before(""_sv, 'x') == make_split(""_sv, ""_sv));
+    REQUIRE(split_before("abc"_sv, 'a') == make_split(""_sv, "abc"_sv));
+    REQUIRE(split_before("abc"_sv, 'b') == make_split("a"_sv, "bc"_sv));
+    REQUIRE(split_before("abc"_sv, 'c') == make_split("ab"_sv, "c"_sv));
+    REQUIRE(split_before("abc"_sv, 'x') == make_split("abc"_sv, ""_sv));
+}
+
+TEST_CASE("split on a delimiter list, leave delimiter in second part")
+{
+    REQUIRE(split_before(""_sv, "x"_sv) == make_split(""_sv, ""_sv));
+
+    REQUIRE(split_before("abc"_sv, "a"_sv) == make_split(""_sv, "abc"_sv));
+    REQUIRE(split_before("abc"_sv, "b"_sv) == make_split("a"_sv, "bc"_sv));
+    REQUIRE(split_before("abc"_sv, "c"_sv) == make_split("ab"_sv, "c"_sv));
+    REQUIRE(split_before("abc"_sv, "x"_sv) == make_split("abc"_sv, ""_sv));
+
+    REQUIRE(split_before("abc"_sv, "ax"_sv) == make_split(""_sv, "abc"_sv));
+    REQUIRE(split_before("abc"_sv, "bx"_sv) == make_split("a"_sv, "bc"_sv));
+    REQUIRE(split_before("abc"_sv, "cx"_sv) == make_split("ab"_sv, "c"_sv));
+    REQUIRE(split_before("abc"_sv, "xy"_sv) == make_split("abc"_sv, ""_sv));
+
+    REQUIRE(split_before("abc"_sv, "xa"_sv) == make_split(""_sv, "abc"_sv));
+    REQUIRE(split_before("abc"_sv, "xb"_sv) == make_split("a"_sv, "bc"_sv));
+    REQUIRE(split_before("abc"_sv, "xc"_sv) == make_split("ab"_sv, "c"_sv));
+}
+
+TEST_CASE("split on a char, leave delimiter in first part")
+{
+    REQUIRE(split_after(""_sv, 'x') == make_split(""_sv, ""_sv));
+    REQUIRE(split_after("abc"_sv, 'a') == make_split("a"_sv, "bc"_sv));
+    REQUIRE(split_after("abc"_sv, 'b') == make_split("ab"_sv, "c"_sv));
+    REQUIRE(split_after("abc"_sv, 'c') == make_split("abc"_sv, ""_sv));
+    REQUIRE(split_after("abc"_sv, 'x') == make_split("abc"_sv, ""_sv));
+
+}
+
+TEST_CASE("split on a delimiter list, leave delimiter in first part")
+{
+    REQUIRE(split_after(""_sv, "x"_sv) == make_split(""_sv, ""_sv));
+    REQUIRE(split_after("abc"_sv, "a"_sv) == make_split("a"_sv, "bc"_sv));
+    REQUIRE(split_after("abc"_sv, "b"_sv) == make_split("ab"_sv, "c"_sv));
+    REQUIRE(split_after("abc"_sv, "c"_sv) == make_split("abc"_sv, ""_sv));
+    REQUIRE(split_after("abc"_sv, "x"_sv) == make_split("abc"_sv, ""_sv));
+
+    REQUIRE(split_after("abc"_sv, "ax"_sv) == make_split("a"_sv, "bc"_sv));
+    REQUIRE(split_after("abc"_sv, "bx"_sv) == make_split("ab"_sv, "c"_sv));
+    REQUIRE(split_after("abc"_sv, "cx"_sv) == make_split("abc"_sv, ""_sv));
+    REQUIRE(split_after("abc"_sv, "xy"_sv) == make_split("abc"_sv, ""_sv));
+
+    REQUIRE(split_after("abc"_sv, "xa"_sv) == make_split("a"_sv, "bc"_sv));
+    REQUIRE(split_after("abc"_sv, "xb"_sv) == make_split("ab"_sv, "c"_sv));
+    REQUIRE(split_after("abc"_sv, "xc"_sv) == make_split("abc"_sv, ""_sv));
+}
 } // namespace
